e47e189bdca9799add4acfff14a4603052289fd7
hanstananda <hanstananda@gmail.com>
2018-09-23 14:19:10 +0800

    Tut 12 create new Housecontract

--- src/main/java/java_bootcamp/HouseContract.java
+++ src/main/java/java_bootcamp/HouseContract.java
@@ -1,5 +1,44 @@
 package java_bootcamp;
 
-public class HouseContract {
+import net.corda.core.contracts.Command;
+import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.Contract;
+import net.corda.core.transactions.LedgerTransaction;
 
+import java.security.PublicKey;
+import java.util.List;
+
+public class HouseContract implements Contract {
+
+    public void verify(LedgerTransaction tx) throws IllegalArgumentException {
+        if(tx.getCommands().size()!=1)
+        {
+            throw new IllegalArgumentException("Transaction must have one command!");
+        }
+        Command command = tx.getCommand(0);
+        List<PublicKey> requiredSigners = command.getSigners();
+        CommandData commandType = command.getValue();
+
+        //impose rules on certain command
+        if (commandType instanceof Register)
+        {
+            //TODO("Registration transaction logic.")
+        }
+        else if (commandType instanceof Transfer){
+            //TODO("Transfer transaction logic.")
+        }
+        else
+        {
+            throw new IllegalArgumentException("Command type not recognized.");
+        }
+        //throw new IllegalArgumentException("Accept nothing"); --> make it denies everything :)
+    }
+    // use this class to make those classes known as command list
+    public class Register implements CommandData{
+
+    }
+
+    public class Transfer implements  CommandData{
+
+    }
 }
